1ea354e743690f1730fbe35c5e65be92f817b870
Ileska <ileska@tutanota.com>
2026-04-10 23:37:52 +0300

    Draw triangle outline

---
+++ draw4corner/misc.h
@@ -0,0 +1,5 @@
+int mapInt(int inp, int input_start, int input_end, int output_start,
+           int output_end);
+double sign(double num);
+double absMy(double num);
+void swap(double *num0, double *num1);
---
+++ draw4corner/structs.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "config.h"
+#include <bits/types/struct_timeval.h>
+
+struct Point {
+  double xx;
+  double yy;
+};
+
+struct Color {
+  unsigned char rr;
+  unsigned char gg;
+  unsigned char bb;
+};
+
+struct Square {
+  double xx;
+  double yy;
+  double hh;
+  double ww;
+
+  double rot;
+};
+
+struct FourCorner {
+  struct Point corners[4];
+};
+
+/*
+ * In trinagle the corners should be sorted by x value
+ */
+struct Triangle {
+  struct Point corners[3];
+};
+
+struct inputEvent {
+  struct timeval time;
+  unsigned short type;
+  unsigned short code;
+  unsigned int value;
+};
